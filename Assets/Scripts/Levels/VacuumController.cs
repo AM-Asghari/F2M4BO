67c2fba6a7ab5e9ff1f64b8219112a4e4306f31d
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-05-19 12:09:32 +0200

    changes to vacuum

--- Assets/Scripts/Levels/VacuumController.cs
+++ Assets/Scripts/Levels/VacuumController.cs
@@ -15,10 +15,21 @@ public class VacuumController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Make vacuum look at mouse position
+        LookAtMouse();
+
+        //Controls
+    }
+
+    private void LookAtMouse()
+    {
+        //Get screen to world position
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.z =  mousePosition.z - transform.position.z;
 
-        //transform.LookAt(new Vector3(mousePosition.x, mousePosition.y, transform.position.x));
+        Debug.Log(mousePosition.ToString());
+
         Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
         transform.rotation = rot;
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
