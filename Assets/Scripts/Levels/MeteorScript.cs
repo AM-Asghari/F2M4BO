9f7c472878a9214823a8f362f987859b28e11d78
mo <mosawer.asg@outlook.com>
2022-05-25 13:12:51 +0200

    Started on MeteoriteMovement script

---
+++ Assets/Scripts/Levels/MeteorScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorScript : MonoBehaviour
+{
+
+    float xAxisSpeed = 0.01f;
+    float zAxisSpeed = -0.005f;
+    bool movingRight = true;
+    //float sinInd = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position = new Vector3(Mathf.Cos(sinInd) * 10, 0, 0);
+        //sinInd += Time.deltaTime * 0.5f; 
+        if(movingRight == true)
+        {
+            transform.position += new Vector3(xAxisSpeed, zAxisSpeed);
+        }
+    }
+}
