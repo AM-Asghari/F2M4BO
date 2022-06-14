345fe65708d34cda8d07b4810b929b6d44702123
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2022-06-15 00:02:07 +0200

    basic dialogue system done

---
+++ Assets/Scripts/HUD/DialogueTextController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueTextController : MonoBehaviour
+{
+    public GameObject txtObject;
+    public TextMeshProUGUI txt;
+
+    public GameObject bg;
+
+    public GameObject nameObject;
+    public TextMeshProUGUI nametxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtObject = GameObject.FindGameObjectWithTag("dialogue_txt");
+        bg = GameObject.FindGameObjectWithTag("dialogue_bg");
+        nameObject = GameObject.FindGameObjectWithTag("dialogue_name");
+
+        txt = txtObject.GetComponent<TextMeshProUGUI>();
+        nametxt = nameObject.GetComponent<TextMeshProUGUI>();
+
+        //Empty text
+        txt.text = "";
+        nametxt.text = "";
+
+        bg.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetText(string text, string name)
+    {
+        //Split the dialogue into seperate words into an array
+        string[] splitText = text.Split(' ');
+
+        StartCoroutine(DisplayText(splitText, name, 0.025f));
+    }
+
+    IEnumerator DisplayText(string[] text, string name, float delay)
+    {
+        txt.text = "";
+        nametxt.text = name;
+
+        for (int i = 0; i < text.Length; i++) //For each word:
+        {
+            foreach (char ch in text[i]) //For each character in the word:
+            {
+                txt.text += ch; //Add the character to the text
+                yield return new WaitForSeconds(delay); //Wait delay
+            }
+            txt.text += " "; //Add a space after a word is finished
+        }
+        yield break;
+    }
+}
